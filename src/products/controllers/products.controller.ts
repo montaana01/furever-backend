1191a88528a6b15fb990c9664923f0ec9713277b
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-07-25 12:11:11 +0300

    feat: use products method at controller

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProductsService } from '../products.service';
 import { Product, Response } from '../../types/productApi';
+import { Products } from '../entities/products.entity';
+import { CreateProductDto } from '../dto/create-product.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @Post()
+  async create(@Body() createDto: CreateProductDto): Promise<Products> {
+    return this.productsService.createProduct(createDto);
+  }
+
   @Get()
-  getProducts(): Response<Product> {
+  getProducts(): Promise<Response<Products>> {
     return this.productsService.getProducts();
   }
 
